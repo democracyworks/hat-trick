9ffc5ea538216822e7fdf9cc3399e683b289d7bc
Wes Morgan <cap10morgan@gmail.com>
2012-04-18 15:27:26 -0600

    remove commented out log call

--- lib/hat_trick/model_methods.rb
+++ lib/hat_trick/model_methods.rb
@@ -17,7 +17,6 @@ module HatTrick
     end
 
     def save_with_hat_trick(*args)
-      # Rails.logger.info "Saving #{self.inspect} with hat trick"
       enable_validation_group HatTrick::ModelMethods.current_validation_group_for(self.class)
       save_without_hat_trick(*args)
     end
