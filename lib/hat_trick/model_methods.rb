c47448fe093109210ebbb903b92d1964dadd5464
Wes Morgan <cap10morgan@gmail.com>
2012-06-05 16:05:47 -0600

    refactor model_methods a bit
    now aliases perform_validations instead of save, which it what we really want

--- lib/hat_trick/model_methods.rb
+++ lib/hat_trick/model_methods.rb
@@ -1,24 +1,36 @@
 module HatTrick
   module ModelMethods
     extend ActiveSupport::Concern
-    mattr_accessor :current_validation_group
+    mattr_accessor :validation_groups
 
     included do
-      alias_method_chain :save, :hat_trick
+      alias_method_chain :perform_validations, :hat_trick
     end
 
     def self.set_current_validation_group_for(klass, validation_group_name)
-      self.current_validation_group ||= {}
-      current_validation_group[klass.to_s.underscore] = validation_group_name
+      self.validation_groups ||= {}
+      validation_groups[klass.to_s.underscore] = validation_group_name
     end
 
     def self.current_validation_group_for(klass)
-      current_validation_group[klass.to_s.underscore]
+      validation_groups[klass.to_s.underscore]
     end
 
-    def save_with_hat_trick(*args)
-      enable_validation_group HatTrick::ModelMethods.current_validation_group_for(self.class)
-      save_without_hat_trick(*args)
+    def perform_validations_with_hat_trick(*args, &block)
+      enable_current_validation_group
+      perform_validations_without_hat_trick(*args, &block)
+    end
+
+    private
+
+    # don't call this method 'current_validation_group', it conflicts with
+    # the gem
+    def current_step_validation_group
+      HatTrick::ModelMethods.current_validation_group_for(self.class)
+    end
+
+    def enable_current_validation_group
+      enable_validation_group current_step_validation_group
     end
   end
 end
