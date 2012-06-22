9f441b33a61943b9e2d6b8ea61188e8c2b26f8d7
Wes Morgan <cap10morgan@gmail.com>
2012-06-22 16:01:12 -0400

    revert "make skipped state of a step persistent"
    This was a can of worms, and is not really needed now that we are only running the current step's include_data callback.

--- lib/hat_trick/step.rb
+++ lib/hat_trick/step.rb
@@ -4,6 +4,7 @@ module HatTrick
   class Step
     attr_reader :step_def, :wizard
     attr_accessor :next_step
+    attr_writer :skipped
 
     delegate :name, :fieldset, :buttons, :repeat_of, :to_sym, :to_s, :as_json,
              :run_after_callback, :run_before_callback, :repeat?,
@@ -19,16 +20,8 @@ module HatTrick
       wizard.session
     end
 
-    def skipped=(skipped)
-      if skipped
-        session["hat-trick.skipped_steps"] << self.to_sym
-      else
-        session["hat-trick.skipped_steps"].delete(self.to_sym)
-      end
-    end
-
     def skipped?
-      @skipped || session["hat-trick.skipped_steps"].include?(self.to_sym)
+      @skipped && !visited?
     end
 
     def visited?
