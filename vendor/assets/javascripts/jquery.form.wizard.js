01b278104ad42af1e655a8f206274b32fb7c9703
Wes Morgan <cap10morgan@gmail.com>
2012-07-19 14:20:04 -0600

    remove some debugging log statements that shouldn't have been committed

--- vendor/assets/javascripts/jquery.form.wizard.js
+++ vendor/assets/javascripts/jquery.form.wizard.js
@@ -401,12 +401,10 @@
       }
 
       if (this.currentStep !== step || step === this.firstStep) {
-        console.log("Made it past the first if statement");
         this.previousStep = this.currentStep;
         this._checkIflastStep(step);
         this.currentStep = step;
         var stepShownCallback = function() {
-          console.log("Triggering step_shown");
           $(this.element).trigger('step_shown', this._state());
         };
         this._animate(this.previousStep, step, stepShownCallback);
